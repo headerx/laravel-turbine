cb7e9ce1da6da9fc650359142ae78f9b50ba4d50
inmanturbo <inman.turbo@gmail.com>
2021-07-03 03:32:43 -0400

    wip

--- import_build.sh
+++ import_build.sh
@@ -10,5 +10,15 @@ rm ./composer.lock
 rm ./package-lock.json
 rm ./yarn.lock
 
-
 composer update
+
+echo "" >> .env
+echo '##############################' >> .env
+echo '# New changes below' >> .env
+echo '##############################' >> .env
+echo "" >> .env
+
+cat .env.example >> .env
+
+php artisan key:generate
+php artisan storage:link
